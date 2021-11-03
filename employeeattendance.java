847a21a3c5b90ac22f849158289d339d8fcc4a96
adhithyaadhi1003 <adhithya1003@gmail.com>
2021-11-03 05:25:39 -0700

    employee

---
+++ employeeattendance.java
@@ -0,0 +1,13 @@
+public class employeeattendance{
+	public static void main(String[] args){
+	int is_present = 1;
+	double empstatus = Math.floor(Math.random()*2);
+	
+	if(is_present == empstatus){
+	System.out.println("employee is present");
+    
+	}else{
+        System.out.println("employee is absent");
+	}
+     }
+}
