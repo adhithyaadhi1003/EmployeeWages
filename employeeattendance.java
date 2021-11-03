a13f8e23c5adc505e7f4d1550c563d135759164b
adhithyaadhi1003 <adhithya1003@gmail.com>
2021-11-03 05:40:02 -0700

    second commit

--- employeeattendance.java
+++ employeeattendance.java
@@ -1,13 +1,21 @@
-public class employeeattendance{
-	public static void main(String[] args){
-	int is_present = 1;
-	double empstatus = Math.floor(Math.random()*2);
-	
-	if(is_present == empstatus){
-	System.out.println("employee is present");
-    
-	}else{
-        System.out.println("employee is absent");
+
+
+public class employeeattendance {
+	public static void main(String[] args) {
+		int Ispresent = 1;
+		double employee = Math.floor(Math.random()*2);
+				System.out.println(employee);	
+		if (employee == Ispresent)
+			{
+				System.out.println("Employee is present");
+			int	wages = 160;
+				System.out.println("the employee wages is " +wages);
+			}
+		else 
+		{
+			int wages =0;
+				System.out.println("Employee is absent");
+				System.out.println("the employee wages is " +wages);
+		}
 	}
-     }
 }
